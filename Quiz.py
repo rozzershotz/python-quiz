0565fd95c824789e56e13d25a88dfe35fd9a0f7d
Reuben Taylor <q13rtaylor@wgsf.net>
2020-05-04 16:32:23 +0100

    first commit

---
+++ Quiz.py
@@ -0,0 +1,42 @@
+import questions
+
+RightAnswers = 0
+WrongAnswers = 0
+
+def check_answer(question, keyboard):
+    return question["CorrectAnswer"].lower() in keyboard.lower()
+
+def show_question(question):
+    global RightAnswers
+    global WrongAnswers
+    print(question["QuestionText"])
+    print("")
+    for answer in question["Answers"]:
+        print(answer)
+        print()
+    keyboard = input("Give me ur answer pls: " )
+    print ()
+
+    if check_answer(question, keyboard):
+        RightAnswers = RightAnswers + 1
+    else:
+        WrongAnswers = WrongAnswers + 1
+
+for question in questions.get_questions():
+    show_question(question)
+
+
+print("You got %d correct but got %d wrong" % (RightAnswers, WrongAnswers))
+if RightAnswers == 0:
+    print("Dismal.")
+if WrongAnswers == 0:
+    print("Woo, I guess.")
+
+Percentage = 100 * float(RightAnswers) / float(RightAnswers + WrongAnswers)
+if WrongAnswers == 0:
+    print("You got 100%")
+if RightAnswers == 0:
+    print("You got 0%")
+if RightAnswers >0:
+    print("You got %d%%" % (Percentage))
+
---
+++ questions.py
@@ -0,0 +1,26 @@
+def get_questions():
+    Questions = []
+
+    Question1 = {}
+    Question1["QuestionText"] = "Where does a fish live?."
+    Question1["Answers"] = ["Water.io", "H2O", "A compound of nutrients supported in a nuclei-based water mixture * You can just put compound if that's your answer*"]
+    Question1["CorrectAnswer"] = "compound"
+    
+    Questions.append(Question1)
+    
+    Question2 = {}
+    Question2["QuestionText"] = "What is a dog."
+    Question2["Answers"] = ["A dog", "A dog", "A dog"]
+    Question2["CorrectAnswer"] = "dog" 
+    
+    Questions.append(Question2)
+    
+    Question3 = {}
+    Question3["QuestionText"] = "Animals are..."
+    Question3["Answers"] = ["Annoying", "Cute", "Not humans"]
+    Question3["CorrectAnswer"] = "Not"
+    
+    Questions.append(Question3)
+
+    return Questions
+
