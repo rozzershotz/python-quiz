0e6db04475d3c3fe6a4669131a4a95e5a6da9c49
Reuben Taylor <q13rtaylor@wgsf.net>
2020-09-02 14:10:31 +0100

    completed comments yay

--- Quiz.py
+++ Quiz.py
@@ -1,13 +1,16 @@
 # main program file, run this for quiz
 
+# imports questions.py
 import questions
 
 RightAnswers = 0
-WrongAnswers = 0
+WrongAnswers = 0 
 
+# checks if the answer from the keyboard contains the right answer 
 def check_answer(question, keyboard):
     return question.correctAnswer.lower() in keyboard.lower()
 
+# displays the question to the user, takes the answer from the user and checks if it is right
 def show_question(question):
     global RightAnswers
     global WrongAnswers
@@ -24,21 +27,23 @@ def show_question(question):
     else:
         WrongAnswers = WrongAnswers + 1
 
+# shows the questions 
 for question in questions.get_questions():
     show_question(question)
 
-
-print("You got %d correct but got %d wrong" % (RightAnswers, WrongAnswers))
+# prints how many you got wrong and right
+print("You got %d correct and got %d wrong" % (RightAnswers, WrongAnswers))
 if RightAnswers == 0:
     print("Dismal.")
 if WrongAnswers == 0:
     print("Woo, I guess.")
 
-Percentage = 100 * float(RightAnswers) / float(RightAnswers + WrongAnswers)
+# prints how much you got in a percentage 
 if WrongAnswers == 0:
     print("You got 100%")
 if RightAnswers == 0:
     print("You got 0%")
-if RightAnswers >0:
+if RightAnswers > 0 and WrongAnswers > 0:
+    Percentage = 100 * float(RightAnswers) / float(RightAnswers + WrongAnswers)
     print("You got %d%%" % (Percentage))
 
